e08d86b266e568d602118b0dbc6b4974b5329e94
Inna Dmytrenko <inna.shalun@gmail.com>
2023-06-08 22:24:26 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,9 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from 'components/App';
+import App from './components/App';
 import { Provider } from 'react-redux';
-import store from './redux/store';
-import './index.css';
+import store from './components/redux/store';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Provider store={store}>
