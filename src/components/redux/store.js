7c74374afa26cf2bb1085fe89f50800efacb262f
Inna Dmytrenko <inna.shalun@gmail.com>
2023-06-08 22:29:46 +0300

    Update store.js

--- src/components/redux/store.js
+++ src/components/redux/store.js
@@ -11,7 +11,7 @@ import {
 } from 'redux-persist';
 import storage from 'redux-persist/lib/storage';
 
-import { reducer } from './reducer';
+import { reducer } from './reduser';
 
 const persistConfig = {
     key: 'contacts',
