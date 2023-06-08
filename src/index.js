67706151a9fe95bdb7b37cc70a69871fbb5fbe30
Inna Dmytrenko <inna.shalun@gmail.com>
2023-06-08 22:18:05 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -6,7 +6,6 @@ import store from './redux/store';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  </React.StrictMode>
   <Provider store={store}>
     <React.StrictMode>
       <App />
